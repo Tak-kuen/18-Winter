9eb8255043ebdae6b568bc976afc11717fc13901
Tak-kuen <32920482+Tak-kuen@users.noreply.github.com>
2018-01-22 19:23:15 +0900

    Algorithm with Python
    18.01.22 Start

---
+++ Algorithm with Python/even_odd.py
@@ -0,0 +1,8 @@
+a=10
+if a % 2 == 0:
+    print(a)
+    print("짝수")
+elif a & 2 == 1:
+    print("홀수")
+else :
+    print("몰라")
---
+++ Algorithm with Python/loop.py
@@ -0,0 +1,15 @@
+i=1
+while i<=10 :
+    print(i)
+    i+=1
+
+for x in range(11):
+    print(x)
+
+for y in range(1,11):
+    print(y)
+
+a={1,3,5,7,9}
+for z in a:
+    print(z)
+
